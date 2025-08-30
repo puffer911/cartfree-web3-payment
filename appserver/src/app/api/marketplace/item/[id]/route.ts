b514e001188d070c58a1e0dbdb8bb545bf10c581
rangga <rangga.sidiq@gmail.com>
2025-08-31 04:59:08 +0700

    Fix build

--- appserver/src/app/api/marketplace/item/[id]/route.ts
+++ appserver/src/app/api/marketplace/item/[id]/route.ts
@@ -10,7 +10,7 @@ export async function GET(
 ) {
 const { params } = context;
   try {
-    const { id } = params;
+    const { id } = await params;
 
     if (!id) {
       return NextResponse.json(
